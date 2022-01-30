be521cdd4fa2443f947622ceac507df826cbe7e1
Kuba <jakub.k.paluszkiewicz@gmail.com>
2022-01-30 12:41:49 +0100

    env, routes, config, ProxyController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProxyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix("proxy")->group(function () {
+    Route::any('/{query}', [ProxyController::class, 'proxy'])
+        ->where('query', "/*");
 });
